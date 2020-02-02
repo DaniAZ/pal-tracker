8c06b4279fac63f69d588a972787434fb66020f2
DaniAZ <da16ni19@gmail.com>
2020-02-02 15:31:50 -0500

    using jdbc template

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -3,6 +3,7 @@ package io.pivotal.pal.tracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @SpringBootApplication
 public class PalTrackerApplication {
@@ -10,9 +11,19 @@ public class PalTrackerApplication {
 
       @Bean
       public  TimeEntryRepository timeEntryRepository(){
-          return new InMemoryTimeEntryRepository();
+          return new JdbcTimeEntryRepository(mysqlDataSource());
           }
 
+
+    @Bean
+    public DriverManagerDataSource mysqlDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+      //  dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl(System.getenv("SPRING_DATASOURCE_URL"));
+
+
+        return dataSource;
+    }
     public static void main(String[] args) {
         SpringApplication.run(PalTrackerApplication.class, args);
     }
